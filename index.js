988612560212bb42c588d37a97727ee1d9f621b4
AxelothLeohryn <alejandro97ss@gmail.com>
2024-03-17 20:25:32 +0100

    add some additional files for backend in production

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.use(morgan("dev"));
 const apiRoutes = require("./routes/api");
 app.use("/api/v1", apiRoutes);
 
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder (VITE --> dist)
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+}
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
